d8614c780d8ce6e8a4633ebbe27792ddc39b1570
Daniel Neuberger <5875881+deuberger@users.noreply.github.com>
2021-05-31 17:48:03 +0000

    Add flask migrations db

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask_api import FlaskAPI
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from instance.config import app_config
 
-# initialize sql-alchemy
 db = SQLAlchemy()
-
+migrate = Migrate()
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
@@ -13,5 +13,6 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    migrate.init_app(app, db)
 
     return app
